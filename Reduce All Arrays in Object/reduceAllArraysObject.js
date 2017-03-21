97be7609f1f4043e129fa37f96b7b1ab02bf703a
Carlo Enrico <carlo.enrico@uol.com.br>
2017-03-21 08:36:12 -0300

    update file

--- Reduce All Arrays in Object/reduceAllArraysObject.js
+++ Reduce All Arrays in Object/reduceAllArraysObject.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const object = {
 
     "attr1": 1,
@@ -13,14 +11,14 @@ const object = {
     "attr5": [{
         "subattr5.1": [21, 22, [23, 24], 25, [26, [27, 28], 29, [30]]]
     }],
-    "attr5": "valor1",
-    "attr6": [{
-        "attr6.1": "valor2",
-        "attr6.2": [{
-            "attr6.2.1": "valor3",
-            "attr6.2.2": [{
-                "attr6.2.2.1": "valor4",
-                "attr6.2.2.2": { "attr6.2.2.2.1": "valor5", "attr6.2.2.2.2": "valor6" }
+    "attr6": "valor1",
+    "attr7": [{
+        "attr7.1": "valor2",
+        "attr7.2": [{
+            "attr7.2.1": "valor3",
+            "attr7.2.2": [{
+                "attr7.2.2.1": "valor4",
+                "attr7.2.2.2": { "attr7.2.2.2.1": "valor5", "attr7.2.2.2.2": "valor6" }
             }]
         }]
     }]
@@ -30,6 +28,4 @@ const isObject = (obj) => obj instanceof Object;
 const objectValues = (obj) => Object.values(obj);
 const flattenObj = (obj) => objectValues(obj).reduce((pre, pos) => pre.concat(isObject(pos) ? flattenObj(pos) : pos), [])
 
-console.log(flattenObj(object)); // [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, "valor1", "valor2", "valor3", "valor4", "valor5", "valor6"]
-
-
+console.log(flattenObj(object)); // [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, "valor1", "valor2", "valor3", "valor4", "valor5", "valor6"]
